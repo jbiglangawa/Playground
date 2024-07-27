28e88a613e9dd2d185dad45ab3bc2b9f550e9f33
John Marvie Biglang-awa <johnmarviebiglangawa@gmail.com>
2024-07-27 12:26:52 +0800

    kafka

--- src/main/java/com/swingspringer/practice/Playground/controller/HealthController.java
+++ src/main/java/com/swingspringer/practice/Playground/controller/HealthController.java
@@ -1,20 +1,27 @@
 package com.swingspringer.practice.Playground.controller;
 
+import com.swingspringer.practice.Playground.kafka.producer.PlaygroundKafkaProducer;
 import com.swingspringer.practice.Playground.models.Health;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/health")
+@RequiredArgsConstructor
 public class HealthController {
     private static final Logger log = Logger.getLogger(HealthController.class.getName());
+    private final PlaygroundKafkaProducer playgroundKafkaProducer;
 
     @GetMapping
     public ResponseEntity<Health> ping() {
+        playgroundKafkaProducer.sendMessage("health", new Date().toString());
         log.info("Ping");
         return ResponseEntity.ok(new Health(true));
     }
---
+++ src/main/java/com/swingspringer/practice/Playground/kafka/consumer/PlaygroundKafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.swingspringer.practice.Playground.kafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlaygroundKafkaConsumer {
+
+    @KafkaListener(topics = "health", groupId = "playground-group-id")
+    public void health(String message) {
+        System.out.println("Received message: for health " + message);
+    }
+
+}
